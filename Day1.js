3271dbc1946dd04a0b8a5c143743e86f2d8fb0e5
Kaylem <kaylem20201@gmail.com>
2023-12-11 19:51:44 -0700

    Day one complete

---
+++ Day1.js
@@ -0,0 +1,82 @@
+import fs from 'node:fs';
+import readline from 'node:readline';
+async function processLineByLine() {
+    const fileStream = fs.createReadStream('inputs/day1.txt');
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity,
+    });
+    const lines = [];
+    for await (const line of rl) {
+        lines.push(line);
+    }
+    return lines;
+}
+const DigitMatch = new RegExp("(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)");
+function convertDigitStringToNumber(input) {
+    switch (input) {
+        case "one":
+            return 1;
+            break;
+        case "two":
+            return 2;
+            break;
+        case "three":
+            return 3;
+            break;
+        case "four":
+            return 4;
+            break;
+        case "five":
+            return 5;
+            break;
+        case "six":
+            return 6;
+            break;
+        case "seven":
+            return 7;
+            break;
+        case "eight":
+            return 8;
+            break;
+        case "nine":
+            return 9;
+            break;
+        default:
+            return NaN;
+    }
+}
+function findFirstDigit(line) {
+    for (let i = 0; i <= line.length; i++) {
+        const substring = line.substring(0, i);
+        const digitResult = substring.match(/\d/);
+        if (digitResult !== null)
+            return Number.parseInt(digitResult[0]);
+        const stringResult = substring.match(DigitMatch);
+        if (stringResult !== null)
+            return convertDigitStringToNumber(stringResult[0]);
+    }
+    return 0;
+}
+function findLastDigit(line) {
+    for (let i = line.length - 1; i >= 0; i--) {
+        const substring = line.substring(i, line.length);
+        const digitResult = substring.match(/\d/);
+        if (digitResult !== null)
+            return Number.parseInt(digitResult[0]);
+        const stringResult = substring.match(DigitMatch);
+        if (stringResult !== null)
+            return convertDigitStringToNumber(stringResult[0]);
+    }
+    return 0;
+}
+let sum = 0;
+const lines = await processLineByLine();
+for (const line of lines) {
+    const firstDigit = findFirstDigit(line);
+    const lastDigit = findLastDigit(line);
+    //console.log(firstDigit*10 + lastDigit);
+    sum += (firstDigit * 10) + lastDigit;
+}
+console.log(sum);
+process.exit(0);
