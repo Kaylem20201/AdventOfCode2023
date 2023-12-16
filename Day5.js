7f641311bbe71ec3e7638a01c88c69f35c0196c5
Kaylem <kaylem20201@gmail.com>
2023-12-16 07:59:48 -0700

    Day 5 Part 2 Complete

--- Day5.js
+++ Day5.js
@@ -1,7 +1,7 @@
 import { inputToLines } from "./util.js";
 const DAYNUMBER = 5;
 const lines = await inputToLines(DAYNUMBER);
-function parseInput(inputLines) {
+function parseInput(inputLines, mode) {
     const SEEDEXP = /seeds:(?:\s*((?:\s*\d+\s*)+)\s*)+/;
     const SEEDTOSOILEXP = /seed\-to\-soil map\:/;
     const SOILTOFERTILIZEREXP = /soil\-to\-fertilizer map\:/;
@@ -30,16 +30,30 @@ function parseInput(inputLines) {
     const temperatureToHumidityMaps = parseMapLines(inputLines, lineIndex, TEMPERATURETOHUMIDITYEXP);
     lineIndex += temperatureToHumidityMaps.length + 2;
     const humidityToLocationMaps = parseMapLines(inputLines, lineIndex, HUMIDITYTOLOCATIONEXP);
+    if (mode === 2) {
+        const result = {
+            seeds: seeds,
+            mapSet: {
+                seedToSoilMaps: sortMapsByDestination(seedToSoilMaps),
+                soilToFertilizerMaps: sortMapsByDestination(soilToFertilizerMaps),
+                fertilizerToWaterMaps: sortMapsByDestination(fertilizerToWaterMaps),
+                waterToLightMaps: sortMapsByDestination(waterToLightMaps),
+                lightToTemperatureMaps: sortMapsByDestination(lightToTemperatureMaps),
+                temperatureToHumidityMaps: sortMapsByDestination(temperatureToHumidityMaps),
+                humidityToLocationMaps: sortMapsByDestination(humidityToLocationMaps)
+            }
+        };
+    }
     const result = {
         seeds: seeds,
         mapSet: {
-            seedToSoilMaps: sortMaps(seedToSoilMaps),
-            soilToFertilizerMaps: sortMaps(soilToFertilizerMaps),
-            fertilizerToWaterMaps: sortMaps(fertilizerToWaterMaps),
-            waterToLightMaps: sortMaps(waterToLightMaps),
-            lightToTemperatureMaps: sortMaps(lightToTemperatureMaps),
-            temperatureToHumidityMaps: sortMaps(temperatureToHumidityMaps),
-            humidityToLocationMaps: sortMaps(humidityToLocationMaps)
+            seedToSoilMaps: sortMapsBySource(seedToSoilMaps),
+            soilToFertilizerMaps: sortMapsBySource(soilToFertilizerMaps),
+            fertilizerToWaterMaps: sortMapsBySource(fertilizerToWaterMaps),
+            waterToLightMaps: sortMapsBySource(waterToLightMaps),
+            lightToTemperatureMaps: sortMapsBySource(lightToTemperatureMaps),
+            temperatureToHumidityMaps: sortMapsBySource(temperatureToHumidityMaps),
+            humidityToLocationMaps: sortMapsBySource(humidityToLocationMaps)
         }
     };
     return result;
@@ -82,21 +96,86 @@ function routeSeedToLocation(seedNumber, mapSet) {
         mapSet.humidityToLocationMaps
     ];
     for (const currentSet of orderedMapSet) {
-        for (const map of currentSet) {
-            if (map.sourceStart > currentResult)
-                continue;
-            if (map.sourceStart + map.range <= currentResult)
-                continue;
-            console.log('Found route. Current: %d, SourceStart: %d, DestinationStart: %d, Range: %d', currentResult, map.sourceStart, map.destinationStart, map.range);
-            currentResult = (map.destinationStart + (currentResult - map.sourceStart));
-            console.log('Calculated new number: %d', currentResult);
-            break;
-        }
+        currentResult = sourceToDestination(currentResult, currentSet);
     }
     return (currentResult);
 }
-function sortMaps(mapArray) {
+function sourceToDestination(startingNumber, mapArray) {
+    let currentResult = startingNumber;
+    for (const map of mapArray) {
+        if (map.sourceStart > currentResult)
+            continue;
+        if (map.sourceStart + map.range <= currentResult)
+            continue;
+        //console.log('Found route. Current: %d, SourceStart: %d, DestinationStart: %d, Range: %d', 
+        //currentResult, map.sourceStart, map.destinationStart, map.range);
+        currentResult = (map.destinationStart + (currentResult - map.sourceStart));
+        //console.log('Calculated new number: %d', currentResult);
+        break;
+    }
+    return currentResult;
+}
+function destinationToStart(startingNumber, mapArray) {
+    let currentResult = startingNumber;
+    for (const map of mapArray) {
+        if (map.destinationStart > currentResult)
+            continue;
+        if (map.destinationStart + map.range <= currentResult)
+            continue;
+        //console.log('Found route. Current: %d, SourceStart: %d, DestinationStart: %d, Range: %d', 
+        //currentResult, map.sourceStart, map.destinationStart, map.range);
+        currentResult = (map.sourceStart + (currentResult - map.destinationStart));
+        //console.log('Calculated new number: %d', currentResult);
+        break;
+    }
+    return currentResult;
+}
+function getBreakpoints(mapSet, seedRanges) {
+    const orderedMapSet = [
+        mapSet.humidityToLocationMaps,
+        mapSet.temperatureToHumidityMaps,
+        mapSet.lightToTemperatureMaps,
+        mapSet.waterToLightMaps,
+        mapSet.fertilizerToWaterMaps,
+        mapSet.soilToFertilizerMaps,
+        mapSet.seedToSoilMaps
+    ];
+    let breakpoints = [];
+    for (const currentSet of orderedMapSet) {
+        //convert old breakpoints
+        breakpoints = breakpoints.map((breakpoint) => {
+            return destinationToStart(breakpoint, currentSet);
+        });
+        //add new breakpoints
+        if (!breakpoints.includes(0))
+            breakpoints.push(0);
+        for (const map of currentSet)
+            breakpoints.push(map.sourceStart);
+    }
+    //Remove any numbers that don't fit in seed ranges
+    breakpoints = breakpoints.filter((seedNumber) => {
+        for (const seedRange of seedRanges) {
+            if (seedRange.start > seedNumber)
+                continue;
+            if (seedRange.start + seedRange.range <= seedNumber)
+                continue;
+            return true;
+        }
+        return false;
+    });
+    //Finally, add the beginning of every seed range
+    for (const seedRange of seedRanges) {
+        if (!breakpoints.includes(seedRange.start))
+            breakpoints.push(seedRange.start);
+    }
+    return breakpoints;
+}
+function sortMapsBySource(mapArray) {
     const sortedArray = [...mapArray].sort((a, b) => a.sourceStart - b.sourceStart);
+    return sortedArray;
+}
+function sortMapsByDestination(mapArray) {
+    const sortedArray = [...mapArray].sort((a, b) => a.destinationStart - b.destinationStart);
     return sortedArray;
 }
 function part1Main() {
@@ -110,4 +189,22 @@ function part1Main() {
     }
     console.log(minimumLocation);
 }
-part1Main();
+function part2Main() {
+    const parsedInput = parseInput(lines, 2); //Sort maps by destination
+    const parsedSeedLine = parsedInput.seeds;
+    const mapSet = parsedInput.mapSet;
+    const seedRanges = new Array(parsedSeedLine.length / 2);
+    for (let i = 0; i < seedRanges.length; i++) {
+        seedRanges[i] = { start: parsedSeedLine[i * 2], range: parsedSeedLine[i * 2 + 1] };
+    }
+    seedRanges.sort((a, b) => a.start - b.start);
+    const breakpoints = getBreakpoints(mapSet, seedRanges);
+    console.log(breakpoints);
+    let minimumLocation = Infinity;
+    for (const breakpoint of breakpoints) {
+        const location = routeSeedToLocation(breakpoint, mapSet);
+        minimumLocation = (location < minimumLocation) ? location : minimumLocation;
+    }
+    console.log(minimumLocation);
+}
+part2Main();
