1eade86bf46e09f824073978cbba2bd38add265e
Kaylem <kaylem20201@gmail.com>
2023-12-17 20:09:08 -0700

    Day7Part2 complete

--- day7/Day7.js
+++ day7/Day7.js
@@ -11,7 +11,7 @@ var HandRank;
     HandRank[HandRank["onePair"] = 2] = "onePair";
     HandRank[HandRank["highCard"] = 1] = "highCard";
 })(HandRank || (HandRank = {}));
-function parseInput(inputLines) {
+function parseInput(inputLines, mode) {
     const HANDEXP = /(\S{5}) (\d+)/;
     const hands = [];
     for (const line of inputLines) {
@@ -19,13 +19,19 @@ function parseInput(inputLines) {
         if (handMatch === null || handMatch.length < 3)
             process.exit(1);
         const cardsRaw = handMatch[1].split('');
-        const cards = cardsRaw.map(convertCharToRank);
+        let cards;
+        if (mode === 2) {
+            cards = cardsRaw.map((card) => convertCharToRank(card, 2));
+        }
+        else {
+            cards = cardsRaw.map(convertCharToRank);
+        }
         const bet = Number.parseInt(handMatch[2]);
         hands.push({ hand: cards, bet: bet });
     }
     return hands;
 }
-function convertCharToRank(character) {
+function convertCharToRank(character, mode) {
     let FaceCards;
     (function (FaceCards) {
         FaceCards[FaceCards["T"] = 10] = "T";
@@ -34,8 +40,26 @@ function convertCharToRank(character) {
         FaceCards[FaceCards["K"] = 13] = "K";
         FaceCards[FaceCards["A"] = 14] = "A";
     })(FaceCards || (FaceCards = {}));
+    let FaceCardsPart2;
+    (function (FaceCardsPart2) {
+        FaceCardsPart2[FaceCardsPart2["T"] = 10] = "T";
+        FaceCardsPart2[FaceCardsPart2["J"] = 1] = "J";
+        FaceCardsPart2[FaceCardsPart2["Q"] = 11] = "Q";
+        FaceCardsPart2[FaceCardsPart2["K"] = 12] = "K";
+        FaceCardsPart2[FaceCardsPart2["A"] = 13] = "A";
+    })(FaceCardsPart2 || (FaceCardsPart2 = {}));
     if (character.length > 1)
         process.exit(1);
+    if (mode === 2) {
+        switch (character) {
+            case 'T': return FaceCardsPart2.T;
+            case 'J': return FaceCardsPart2.J;
+            case 'Q': return FaceCardsPart2.Q;
+            case 'K': return FaceCardsPart2.K;
+            case 'A': return FaceCardsPart2.A;
+            default: return Number.parseInt(character);
+        }
+    }
     switch (character) {
         case 'T': return FaceCards.T;
         case 'J': return FaceCards.J;
@@ -58,32 +82,73 @@ function sortHands(handA, handB) {
     }
     return 0;
 }
-function calculateHandRank(hand) {
-    const cards = hand.hand;
+function sortHandsPart2(handA, handB) {
+    const handARank = calculateHandRank(handA, 2);
+    const handBRank = calculateHandRank(handB, 2);
+    if (handARank !== handBRank)
+        return handARank - handBRank;
+    //Same rank, find first higher card
+    for (let i = 0; i < 5; i++) {
+        const diff = handA.hand[i] - handB.hand[i];
+        if (diff !== 0)
+            return diff;
+    }
+    return 0;
+}
+function calculateHandRank(hand, mode) {
+    let jokers;
+    let cards;
+    if (mode === 2) {
+        jokers = hand.hand.filter((filterCard) => filterCard === 1).length;
+        cards = hand.hand.filter((filterCard) => filterCard !== 1);
+    }
+    else {
+        cards = hand.hand;
+        jokers = 0;
+    }
     const cardMatches = [];
     for (const card of cards) {
         cardMatches.push(cards.filter((filterCard) => card === filterCard).length);
     }
+    let resultRank;
     //FiveOfAKind
     if (cardMatches.includes(5))
-        return HandRank.fiveOfAKind;
+        resultRank = HandRank.fiveOfAKind;
     //FourOfAKind
-    if (cardMatches.includes(4))
-        return HandRank.fourOfAKind;
+    else if (cardMatches.includes(4))
+        resultRank = HandRank.fourOfAKind;
     //FullHouse
-    if (cardMatches.includes(3) && cardMatches.includes(2))
-        return HandRank.fullHouse;
+    else if (cardMatches.includes(3) && cardMatches.includes(2))
+        resultRank = HandRank.fullHouse;
     //ThreeOfAKind
-    if (cardMatches.includes(3))
-        return HandRank.threeOfAKind;
+    else if (cardMatches.includes(3))
+        resultRank = HandRank.threeOfAKind;
     //TwoPair
-    if (cardMatches.filter((match) => { return match === 2; }).length === 4)
-        return HandRank.twoPair;
+    else if (cardMatches.filter((match) => { return match === 2; }).length === 4)
+        resultRank = HandRank.twoPair;
     //OnePair
-    if (cardMatches.includes(2))
-        return HandRank.onePair;
+    else if (cardMatches.includes(2))
+        resultRank = HandRank.onePair;
     //HighCard
-    return HandRank.highCard;
+    else
+        resultRank = HandRank.highCard;
+    //Joker adjustment
+    while (jokers > 0) {
+        if (resultRank === HandRank.threeOfAKind) {
+            resultRank += 2;
+        }
+        else if (resultRank === HandRank.onePair) {
+            resultRank += 2;
+        }
+        else if (resultRank === HandRank.twoPair) {
+            resultRank = HandRank.fullHouse;
+        }
+        else {
+            resultRank++;
+        }
+        jokers--;
+    }
+    return (Math.min(resultRank, HandRank.fiveOfAKind));
 }
 function part1Main(inputLines) {
     const hands = parseInput(inputLines);
@@ -101,7 +166,25 @@ function part1Main(inputLines) {
     for (let i = 0; i < sortedHands.length; i++) {
         sum += sortedHands[i].bet * (i + 1);
     }
+    console.log(sum);
+}
+function part2Main(inputLines) {
+    const hands = parseInput(inputLines, 2);
+    //const sortedHands : HandInfo[] = [];
+    const sortedHands = hands.sort(sortHandsPart2);
+    /*
+    for (const hand of hands) {
+        if (sortedHands.length === 0) {
+            sortedHands.push(hand);
+            continue;
+        }
+        sortedHands.splice(binaryInsertionSearch(sortedHands,hand,0,sortedHands.length-1,sortHands),0,hand);
+    }*/
+    let sum = 0;
+    for (let i = 0; i < sortedHands.length; i++) {
+        sum += sortedHands[i].bet * (i + 1);
+    }
     console.log(sortedHands);
     console.log(sum);
 }
-part1Main(lines);
+part2Main(lines);
--- day7/Day7.ts
+++ day7/Day7.ts
@@ -5,8 +5,8 @@ const DAYNUMBER = 7;
 const lines = await inputToLines(DAYNUMBER);
 
 interface HandInfo {
-    hand : number[],
-    bet : number
+    hand: number[],
+    bet: number
 }
 
 enum HandRank {
@@ -19,25 +19,27 @@ enum HandRank {
     highCard = 1
 }
 
-function parseInput(inputLines : string[]) : HandInfo[] {
+function parseInput(inputLines: string[], mode?: number): HandInfo[] {
 
     const HANDEXP = /(\S{5}) (\d+)/;
 
-    const hands : HandInfo[] = [];
+    const hands: HandInfo[] = [];
     for (const line of inputLines) {
         const handMatch = line.match(HANDEXP);
         if (handMatch === null || handMatch.length < 3) process.exit(1);
         const cardsRaw = handMatch[1].split('');
-        const cards  = cardsRaw.map( convertCharToRank );
-        const bet : number = Number.parseInt(handMatch[2]);
-        hands.push({ hand: cards, bet : bet });
+        let cards;
+        if (mode === 2) { cards = cardsRaw.map((card) => convertCharToRank(card, 2)); }
+        else { cards = cardsRaw.map(convertCharToRank); }
+        const bet: number = Number.parseInt(handMatch[2]);
+        hands.push({ hand: cards, bet: bet });
     }
 
     return hands;
 
 }
 
-function convertCharToRank(character : string) : number {
+function convertCharToRank(character: string, mode?: number): number {
 
     enum FaceCards {
         T = 10,
@@ -47,20 +49,39 @@ function convertCharToRank(character : string) : number {
         A = 14
     }
 
+    enum FaceCardsPart2 {
+        T = 10,
+        J = 1,
+        Q = 11,
+        K = 12,
+        A = 13
+    }
+
     if (character.length > 1) process.exit(1);
 
+    if (mode === 2) {
+        switch (character) {
+            case 'T': return FaceCardsPart2.T;
+            case 'J': return FaceCardsPart2.J;
+            case 'Q': return FaceCardsPart2.Q;
+            case 'K': return FaceCardsPart2.K;
+            case 'A': return FaceCardsPart2.A;
+            default: return Number.parseInt(character);
+        }
+    }
+
     switch (character) {
-        case 'T':   return FaceCards.T;
-        case 'J':   return FaceCards.J;
-        case 'Q':   return FaceCards.Q;
-        case 'K':   return FaceCards.K;
-        case 'A':   return FaceCards.A;
-        default:    return Number.parseInt(character);
+        case 'T': return FaceCards.T;
+        case 'J': return FaceCards.J;
+        case 'Q': return FaceCards.Q;
+        case 'K': return FaceCards.K;
+        case 'A': return FaceCards.A;
+        default: return Number.parseInt(character);
     }
 
 }
 
-function sortHands(handA : HandInfo, handB : HandInfo) : number {
+function sortHands(handA: HandInfo, handB: HandInfo): number {
 
     const handARank = calculateHandRank(handA);
     const handBRank = calculateHandRank(handB);
@@ -77,43 +98,82 @@ function sortHands(handA : HandInfo, handB : HandInfo) : number {
 
 }
 
-function calculateHandRank(hand : HandInfo) : HandRank {
-    
-    const cards = hand.hand;
-    
-    const cardMatches = [];
-    for (const card of cards) {
-        cardMatches.push(cards.filter( (filterCard) => card === filterCard).length);
+function sortHandsPart2(handA: HandInfo, handB: HandInfo): number {
+
+    const handARank = calculateHandRank(handA,2);
+    const handBRank = calculateHandRank(handB,2);
+
+    if (handARank !== handBRank) return handARank - handBRank;
+
+    //Same rank, find first higher card
+    for (let i = 0; i < 5; i++) {
+        const diff = handA.hand[i] - handB.hand[i];
+        if (diff !== 0) return diff;
     }
-    
-    //FiveOfAKind
-    if (cardMatches.includes(5)) return HandRank.fiveOfAKind;
 
-    //FourOfAKind
-    if (cardMatches.includes(4)) return HandRank.fourOfAKind;
-
-    //FullHouse
-    if (cardMatches.includes(3) && cardMatches.includes(2)) return HandRank.fullHouse;
-
-    //ThreeOfAKind
-    if (cardMatches.includes(3)) return HandRank.threeOfAKind;
-
-    //TwoPair
-    if (cardMatches.filter( (match) => {return match === 2}).length === 4) return HandRank.twoPair;
-
-    //OnePair
-    if (cardMatches.includes(2)) return HandRank.onePair;
-
-    //HighCard
-    return HandRank.highCard;
+    return 0;
 
 }
 
-function part1Main(inputLines : string[]) {
+function calculateHandRank(hand: HandInfo, mode?: number): HandRank {
 
-    const hands : HandInfo[] = parseInput(inputLines);
+    let jokers;
+    let cards;
+    if (mode === 2) {
+        jokers = hand.hand.filter((filterCard) => filterCard === 1).length;
+        cards = hand.hand.filter((filterCard) => filterCard !== 1);
+    }
+    else {
+        cards = hand.hand;
+        jokers = 0;
+    }
+
+    const cardMatches = [];
+    for (const card of cards) {
+        cardMatches.push(cards.filter((filterCard) => card === filterCard).length);
+    }
+    
+    let resultRank;
+
+    //FiveOfAKind
+    if (cardMatches.includes(5)) resultRank = HandRank.fiveOfAKind;
+
+    //FourOfAKind
+    else if (cardMatches.includes(4)) resultRank = HandRank.fourOfAKind;
+
+    //FullHouse
+    else if (cardMatches.includes(3) && cardMatches.includes(2)) resultRank = HandRank.fullHouse;
+
+    //ThreeOfAKind
+    else if (cardMatches.includes(3)) resultRank = HandRank.threeOfAKind;
+
+    //TwoPair
+    else if (cardMatches.filter((match) => { return match === 2 }).length === 4) resultRank = HandRank.twoPair;
+
+    //OnePair
+    else if (cardMatches.includes(2)) resultRank = HandRank.onePair;
+
+    //HighCard
+    else resultRank = HandRank.highCard;
+
+    //Joker adjustment
+    while (jokers > 0) {
+        if (resultRank === HandRank.threeOfAKind) { resultRank+=2; }
+        else if (resultRank === HandRank.onePair) { resultRank+=2; }
+        else if (resultRank === HandRank.twoPair) { resultRank=HandRank.fullHouse; }
+        else { resultRank ++; }
+        jokers--;
+    }
+
+    return (Math.min(resultRank, HandRank.fiveOfAKind));
+
+}
+
+function part1Main(inputLines: string[]) {
+
+    const hands: HandInfo[] = parseInput(inputLines);
     //const sortedHands : HandInfo[] = [];
-    const sortedHands : HandInfo[] = hands.sort(sortHands);
+    const sortedHands: HandInfo[] = hands.sort(sortHands);
 
     /*
     for (const hand of hands) {
@@ -126,11 +186,34 @@ function part1Main(inputLines : string[]) {
 
     let sum = 0;
     for (let i = 0; i < sortedHands.length; i++) {
-        sum += sortedHands[i].bet * (i+1);
+        sum += sortedHands[i].bet * (i + 1);
+    }
+    console.log(sum);
+
+}
+
+function part2Main(inputLines: string[]) {
+
+    const hands: HandInfo[] = parseInput(inputLines, 2);
+    //const sortedHands : HandInfo[] = [];
+    const sortedHands: HandInfo[] = hands.sort(sortHandsPart2);
+
+    /*
+    for (const hand of hands) {
+        if (sortedHands.length === 0) {
+            sortedHands.push(hand);
+            continue;
+        }
+        sortedHands.splice(binaryInsertionSearch(sortedHands,hand,0,sortedHands.length-1,sortHands),0,hand);
+    }*/
+
+    let sum = 0;
+    for (let i = 0; i < sortedHands.length; i++) {
+        sum += sortedHands[i].bet * (i + 1);
     }
     console.log(sortedHands);
     console.log(sum);
 
 }
 
-part1Main(lines);
+part2Main(lines);
