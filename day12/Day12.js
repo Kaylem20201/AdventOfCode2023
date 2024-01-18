1477804ef64ffa1213f4274b0f93172141377bbe
Kaylem <kaylem20201@gmail.com>
2024-01-18 10:07:14 -0700

    Added function for unfolding the inputs. Next will be implementing recursion, and then Memoization

--- day12/Day12.js
+++ day12/Day12.js
@@ -1,8 +1,6 @@
 import { inputToLines } from '../util.js';
-import { Debug } from '../util.js';
 const DAYNUMBER = 12;
 const lines = await inputToLines(DAYNUMBER);
-const debug = new Debug();
 var TileTypes;
 (function (TileTypes) {
     TileTypes["good"] = ".";
@@ -20,6 +18,14 @@ function parseInput(inputLines) {
         puzzles.push(puzzle);
     }
     return { puzzles };
+}
+function unfoldLine(inputLine) {
+    const stringParts = inputLine.split(' ');
+    const originalTileString = stringParts[0];
+    const originalHintString = stringParts[1];
+    const newTileString = originalTileString.concat(("?" + originalTileString).repeat(4));
+    const newHintString = originalHintString.concat(("," + originalHintString).repeat(4));
+    return newTileString + " " + newHintString;
 }
 function parseTile(character) {
     switch (character) {
@@ -104,66 +110,9 @@ function consumeFirstHint(puzzle) {
         return true;
     }
 }
-function isValidArrangement(puzzle) {
-    const tiles = [...puzzle.tiles];
-    const hints = [...puzzle.hints];
-    for (let i = 0; i < tiles.length; i++) {
-        if (tiles[i] === TileTypes.unknown)
-            throw new Error();
-        if (tiles[i] === TileTypes.good)
-            continue;
-        if (hints.length === 0)
-            return false;
-        const hintRange = tiles.slice(i, i + hints[0]);
-        if (!hintRange.every((tile) => tile === TileTypes.bad))
-            return false;
-        const indexAfter = i + hints[0];
-        const tileAfter = tiles[indexAfter];
-        if (tileAfter === TileTypes.bad)
-            return false;
-        //Valid hint depleted 
-        i = indexAfter - 1;
-        hints.shift();
-    }
-    if (hints.length > 0)
-        return false;
-    return true;
-}
-function convertUnknowns(puzzle) {
-    const possWithUnknowns = [puzzle];
-    const convertedPossibilites = [];
-    while (possWithUnknowns.length > 0) {
-        const poss = possWithUnknowns.shift();
-        if (poss === undefined)
-            throw new Error();
-        const converted = convertSingleUnknown(poss);
-        if (converted === undefined) {
-            //No unknown tiles left
-            convertedPossibilites.push(poss);
-            continue;
-        }
-        //Some unknowns may be left
-        possWithUnknowns.push(...converted);
-    }
-    return convertedPossibilites;
-}
-function convertSingleUnknown(puzzle) {
-    const tiles = puzzle.tiles;
-    const hints = puzzle.hints;
-    if (!tiles.includes(TileTypes.unknown))
-        return undefined;
-    const firstUnknownIndex = tiles.indexOf(TileTypes.unknown);
-    const goodTiles = tiles.slice(0, firstUnknownIndex).concat(TileTypes.good, tiles.slice(firstUnknownIndex + 1));
-    const goodPoss = { tiles: goodTiles, hints: hints };
-    const badTiles = tiles.slice(0, firstUnknownIndex).concat(TileTypes.bad, tiles.slice(firstUnknownIndex + 1));
-    const badPoss = { tiles: badTiles, hints: hints };
-    const result = [goodPoss, badPoss];
-    return result;
-}
 function part1Main(inputLines) {
     const input = parseInput(inputLines);
     const puzzles = input.puzzles;
-    debug.print(puzzles);
     let sum = 0;
     let lineNo = 1;
     for (const puzzle of puzzles) {
